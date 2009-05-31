92072f7bb85f376805eab8ce3725d33a177aa40e
Felix Bechstein <f@ub0r.de>
2009-05-31 20:04:23 +0200

    init CallMeter NG

---
+++ src/de/ub0r/de/android/callMeterNG/CallMeter.java
@@ -0,0 +1,13 @@
+package de.ub0r.de.android.callMeterNG;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CallMeter extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
