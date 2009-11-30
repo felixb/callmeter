df8bb58ddddebc948e37e671d5d25d173fc44985
Felix Bechstein <f@ub0r.de>
2009-11-30 19:01:24 +0100

    remove ads for ronny t.

--- src/de/ub0r/de/android/callMeterNG/CallMeter.java
+++ src/de/ub0r/de/android/callMeterNG/CallMeter.java
@@ -169,7 +169,8 @@ public class CallMeter extends Activity {
 			"2c72e52ef02a75210dc6680edab6b75d", // Danny S.
 			"f39b49859c04e6ea7849b43c73bd050e", // Łukasz M.
 			"225905ca10fd56ae9c4b82254fa6d490", // George K.
-			"9e30468a2b516aac2d1ddf2a875ca8b8" // Alfons V.
+			"9e30468a2b516aac2d1ddf2a875ca8b8", // Alfons V.
+			"4bf7f35515fb7306dc7c43c9fa88558c" // Ronny T.
 	};
 
 	/**
