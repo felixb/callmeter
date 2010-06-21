3da9e1dd0477d14b064b249806780b8883044dae
Felix Bechstein <f@ub0r.de>
2010-06-21 21:26:55 +0200

    new strings for donation

--- shell/getMissingStrings.sh
+++ shell/getMissingStrings.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-grep -o 'name=[^>]*' res/values/strings.xml > /tmp/callmeter.strings.names
+grep -ho 'name=[^>]*' res/values/strings.xml > /tmp/callmeter.strings.names.orig
+grep -ho 'name=[^>]*' ../ub0rlib/res/values/strings.xml | grep -vf /tmp/callmeter.strings.names.orig > /tmp/callmeter.strings.names
+cat /tmp/callmeter.strings.names.orig >> /tmp/callmeter.strings.names
+rm /tmp/callmeter.strings.names.orig
 
 for f in $(ls res/values-*/strings.xml) ; do
 	l=$(echo $(basename $(dirname $f)) | cut -d'-' -f2)
@@ -14,33 +17,44 @@ for f in $(ls res/values-*/strings.xml) ; do
 	grep 'Copyright (C)' $f >> /tmp/callmeter.strings.$l
 	sed -n '4,/\<resources\>/p' res/values/strings.xml >> /tmp/callmeter.strings.$l
 	for s in $(cat /tmp/callmeter.strings.names) ; do
-		if $(grep -n "$s" res/values/strings.xml | grep -v 'string-array' > /dev/null) ; then
-			grep "$s" $f >> /tmp/callmeter.strings.$l
+		if ( grep "$s" $f | grep -vq '<!-- ' ) ; then
+			if (grep -h "$s" ../ub0rlib/res/values/strings.xml res/values/strings.xml | grep -q 'string-array') ; then
+				pos=$(grep -n "$s" $f | cut -d: -f1)
+				if [ -n "$pos" ] ; then
+					sed -n $f -e "$pos,/<\/string-array>/p" >> /tmp/callmeter.strings.$l
+				fi
+			else
+				grep "$s" $f | head -n1 >> /tmp/callmeter.strings.$l
+			fi
 		else
-			pos=$(grep -n "$s" $f | cut -d: -f1)
-			if [ -n "$pos" ] ; then
-				sed -n $f -e "$pos,/<\/string-array>/p" >> /tmp/callmeter.strings.$l
+			if (grep -h "$s" ../ub0rlib/res/values/strings.xml res/values/strings.xml | grep -q 'string-array') ; then
+				sed -n ../ub0rlib/res/values/strings.xml res/values/strings.xml -e "/$s/,/<\/string-array>/p" | \
+					sed -e 's:^:<!-- :' -e 's:$: -->:' >> /tmp/callmeter.strings.$l
+			else
+				grep -h "$s" ../ub0rlib/res/values/strings.xml res/values/strings.xml | tail -n1 | \
+					sed -e 's:^:<!-- :' -e 's:$: -->:' >> /tmp/callmeter.strings.$l
 			fi
 		fi
 	done
 	echo '</resources>' >> /tmp/callmeter.strings.$l
 	mv /tmp/callmeter.strings.$l $f
 	
-	for s in $(grep -vf /tmp/callmeter.strings.names.$l /tmp/callmeter.strings.names) ; do
-		echo $s >&2
-		pos=$(grep -n "$s" res/values/strings.xml | cut -d: -f1)
-		if $(grep -n "$s" res/values/strings.xml | grep -v 'string-array' > /dev/null) ; then
-			line=$(grep "$s" res/values/strings.xml | sed -e 's:^:<!-- :' -e 's:$: -->:')
-			sed -e "${pos}i${line//\\/\\\\}" -i $f
-		else
-			line=$(sed -n res/values/strings.xml -e "$pos,/<\/string-array>/p" | sed -e 's:^:<!-- :' -e 's:$: -->:')
-			line=$(echo $line | sed -e 's: <!--:\\n<!--:g')
-			sed -e "${pos}i${line}" -i $f
-		fi
-	done
+	#for s in $(grep -vf /tmp/callmeter.strings.names.$l /tmp/callmeter.strings.names) ; do
+	#	echo $s >&2
+	#	pos=$(grep -n "$s" res/values/strings.xml | cut -d: -f1)
+	#	if $(grep -n "$s" res/values/strings.xml | grep -v 'string-array' > /dev/null) ; then
+	#		line=$(grep "$s" res/values/strings.xml | sed -e 's:^:<!-- :' -e 's:$: -->:')
+	#		sed -e "${pos}i${line//\\/\\\\}" -i $f
+	#	else
+	#		line=$(sed -n res/values/strings.xml -e "$pos,/<\/string-array>/p" | sed -e 's:^:<!-- :' -e 's:$: -->:')
+	#		line=$(echo $line | sed -e 's: <!--:\\n<!--:g')
+	#		sed -e "${pos}i${line}" -i $f
+	#	fi
+	#done
 
 	sed 	-e 's:[ 	]*<string:	<string:' \
 		-e 's:[ 	]*</string-:	</string-:' \
 		-e 's:[ 	]*<item>:		<item>:' \
+		-e 's: *$::' \
 		-i $f
 done
