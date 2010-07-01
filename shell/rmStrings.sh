8e48bf3034db8db54beb57fc348057cb2ef1c216
Felix Bechstein <f@ub0r.de>
2010-07-01 07:28:49 +0200

    move scripts to ub0rlib

--- shell/rmStrings.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for p in $@ ; do
-	echo "remove string: $p"
-	for f in $(ls res/values-*/strings.xml) ; do
-		grep -Fv "name=\"${p}\"" $f > /tmp/callmeter.rmStrings
-		mv /tmp/callmeter.rmStrings $f
-	done
-done
