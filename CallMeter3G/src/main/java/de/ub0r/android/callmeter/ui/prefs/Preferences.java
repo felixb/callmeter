63a21e16c409a0276d758eea3b26a053fd123554
Felix Bechstein <f@ub0r.de>
2014-03-26 15:21:51 +0700

    fix closing dialog after importing; issue #959

--- CallMeter3G/src/main/java/de/ub0r/android/callmeter/ui/prefs/Preferences.java
+++ CallMeter3G/src/main/java/de/ub0r/android/callmeter/ui/prefs/Preferences.java
@@ -662,14 +662,14 @@ public final class Preferences extends TrackingSherlockPreferenceActivity implem
 
                             @Override
                             protected void onPostExecute(final Boolean result) {
-                                if (!result) {
-                                    try {
+                                try {
+                                    if (!result) {
                                         Toast.makeText(context, R.string.err_export_read,
                                                 Toast.LENGTH_LONG).show();
-                                        d1.dismiss();
-                                    } catch (Exception e) {
-                                        Log.w(TAG, "activity already finished?", e);
                                     }
+                                    d1.dismiss();
+                                } catch (Exception e) {
+                                    Log.w(TAG, "activity already finished?", e);
                                 }
                                 Preferences.this.checkSimplePrefs();
                             }
