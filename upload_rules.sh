9a80250a78f5ef5b1a89ce4f7b7ed6330f2f670c
Felix Bechstein <f@ub0r.de>
2014-12-24 16:37:45 +0100

    add upload rules script

---
+++ upload_rules.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+if [ ! -d "${1}" ] ; then
+  echo "usage: ${0} new-rules-dir"
+  exit 1
+fi
+
+BASE=$(dirname "${0}")
+RULES=${BASE}/rulesets
+
+mv ${1}/ruleset*xml "${RULES}/"
+pushd "${RULES}/"
+
+for f in $(ls -1 ruleset*xml) ; do
+  dos2unix "$f"
+  ./rename.sh "$f"
+done
+
+./buildjson.py > ../_data/rulesets.json
+
+cd ..
+
+git add _data/rulesets.json rulesets/*xml
+git diff --staged
+
+echo "commit?"
+read a
+git commit -m 'add new rules'
+
+echo "push?"
+read a
+git push
+
